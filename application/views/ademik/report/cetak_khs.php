b31c3814a262207e3e8b3ed5b718358df2f418f5
dulumina <fikri.dulumina@gmail.com>
2021-08-20 13:45:12 +0800

    80082021-pra_pmmdk

--- application/views/ademik/report/cetak_khs.php
+++ application/views/ademik/report/cetak_khs.php
@@ -46,7 +46,7 @@
 								</th>
 								<th style="vertical-align:middle; text-align: center;" class="col-md-11 text-center">
 									<h4><b>
-										KEMENTERIAN PENDIDIKAN DAN KEBUDAYAAN
+										KEMENTERIAN PENDIDIKAN, KEBUDAYAAN, RISET, DAN TEKNOLOGI
 									</b></h4>
 									<h3><b>
 										UNIVERSITAS TADULAKO
