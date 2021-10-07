549279d22a70325aab56e6e689bc932d3c774370
Dzulfikri <fikri.dulumina@gmail.com>
2021-10-08 07:07:16 +0800

    Fix:count Dosen

---
+++ application/models/FeederRunWS.php
@@ -0,0 +1,140 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class FeederRunWS extends CI_Model{
+
+
+  public function __construct(){
+    parent::__construct();
+    $this->feeder['url']=$this->config->item('url_feeder2');
+    $this->feeder['username']=$this->config->item('user_feeder');
+    $this->feeder['password']=$this->config->item('password_feeder');
+  }
+
+  /**
+    * configurasi feeder
+    *
+    * @var array
+    */
+  private $feeder = [];
+  
+  private function runWS($data)
+  {
+
+    $url = $this->feeder['url'];
+    $ch = curl_init();
+      
+    curl_setopt($ch, CURLOPT_POST, 1);
+    
+    $headers = array();
+    
+    $headers[] = 'Content-Type: application/json';
+    
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    
+    $data = json_encode($data);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+    
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+  }
+
+  private function token()
+  {
+   
+    $res = $this->runWS(array(
+     'act' => 'GetToken',
+     'username' => $this->feeder['username'],
+     'password' => $this->feeder['password']
+    ));
+    
+    return json_decode($res)->data->token;
+  }
+  /**
+   * fungsi get data from feeder
+   *
+   * @param string $act
+   * @param string $filter
+   * @param string $limit
+   * @param string $offset
+   * @return object
+   */
+  public function get($act,$filter='',$limit='',$offset='')
+  {
+
+    $params = array(
+      'act'    =>$act,
+      'token'  => $this->token(),
+      'filter' => $filter,
+      'limit'  => $limit,
+      'offset' => $offset
+    );
+
+    return json_decode($this->runWS($params));
+  }
+
+  /**
+   * fungsi insert feeder
+   * 
+   * @param string $act
+   * @param array $record
+   * @return object
+   */
+  public function insert($act,$record)
+  {
+
+    $params = array(
+      'act'    => $act,
+      'token'  => $this->token(),
+      'record' => $record 
+    );
+
+    return json_decode($this->runWS($params));
+  }
+
+  /**
+   * fungsi update data feeder
+   *
+   * @param string $act
+   * @param string $key
+   * @param array $record
+   * @return object
+   */
+  public function update($act,$key,$record)
+  {
+
+    $params = array(
+      'act'    => $act,
+      'token'  => $this->token(),
+      'key'    => $key,
+      'record' => $record
+    );
+
+    return json_decode($this->runWS($params));
+  }
+
+  /**
+   * fungsi delete data feeder
+   *
+   * @param string $act
+   * @param string $key
+   * @return object
+   */
+  public function delete($act,$key)
+  {
+
+    $params = array(
+      'act'    => $act,
+      'token'  => $this->token(),
+      'key'    => $key
+    );
+
+    return json_decode($this->runWS($params));
+  }
+
+}
+?>
