9235e867302240023ab8b3b1341b3c1d4335bb1e
fikri dulumina <fikri.dulumina@untad.ac.id>
2022-03-31 03:41:55 +0000

    Master

---
+++ application/models/ws/prodi_model.php
@@ -0,0 +1,25 @@
+<?php
+defined('BASEPATH') OR exit ('No direct script access allowed');
+class Prodi_model extends CI_Model{
+
+    public function Get_data($KodeFakultas='',$Nama_Indonesia=''){
+    
+    if ($KodeFakultas){
+        $this->db->where('KodeFakultas',$KodeFakultas);
+    }
+    // if ($Nama_Indonesia){
+    //     $this->db->where('Nama_Indonesia',$Nama_Indonesia);
+    // }
+    
+    $this->db->select('Kode,Nama_Indonesia,Nama_English,Jenjang');
+    return $this->db->get('_v2_jurusan')->result_array();
+    
+    // $this->db->from('_v2_jurusan');
+    // $this->db->join('_v2_jenjangps','_v2_jenjangps.Kode=_v2_jurusan.Kode');
+    // $this->db->where($NotActive);
+    // $query = $this->db->get();
+    // return $query->result();
+    
+    }
+}
+?>
